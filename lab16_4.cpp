2aae4724c28ee6f4e478ac7158b3ceedc8e4ec1b
Holo <nhuneqw@gmail.com>
2023-02-07 15:28:28 +0700

    lab16_complete

--- lab16_4.cpp
+++ lab16_4.cpp
@@ -17,3 +17,37 @@ int main(){
 	
 	return 0;
 }
+
+void shuffle(int &A,int &B,int &C,int &D){
+	int a[4];
+	a[0] = A , a[1] = B , a[2] = C , a[3] =D;
+
+	int b[4];
+
+	int x=rand()%4;
+	b[0]=a[x];
+	a[x]=0;
+
+	int y=0;
+	do{
+		y=rand()%4;
+	}while(a[y]==0);
+	b[1]=a[y];
+	a[y]=0;
+
+	int z=0;
+	do{
+		z=rand()%4;
+	}while(a[z]==0);
+	b[2]=a[z];
+	a[z]=0;
+
+	int p=0;
+	do{
+		p=rand()%4;
+	}while(a[p]==0);
+	b[3]=a[p];
+	a[p]=0;
+
+	A=b[0],B=b[1],C=b[2],D=b[3];
+}
